92e661bc54edd86a2bf50a0e20204381ff592c25
Stefan Scorse <stefan.scorse.92@gmail.com>
2023-12-04 17:24:47 +1100

    update for python anywhere

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from app import create_app
 
 """
-pipenv run flask --debug run -h 0.0.0.0
+pipenv run flask --debug run -h 0.0.0.0 | to run it locally on your machine
 """
 
 if __name__ == "__main__":
     create_app().run(debug=True)
+else:
+    app = create_app()
